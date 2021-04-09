d6b6502510a05272864f88af8e13609d5140ce9b
trechoy <77rgh77@gmail.com>
2021-04-09 14:55:48 -0400

    implemented flask

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import flask
 from flask import request
-from sqlite3_helper import *
+import sqlite3_helper
+
+db = sqlite3_helper.SqliteDB(db_path='data/database.db', row_type='dict', placeholder='$',
+                             schema_path='data/schema.sql')
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def login():
+    return flask.render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8001, host='127.0.0.1', debug=True, use_evalex=False)
